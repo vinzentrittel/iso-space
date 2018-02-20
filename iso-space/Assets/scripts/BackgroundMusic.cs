369b62152b124797dd0e514fba159d7beb2f9282
Joseph_7695 <jtfq7695@gmail.com>
2018-02-20 16:27:50 +0800

    [+] Menu theme music & Tidying
    Menu theme music added, some tidying
    
    Changelist:
    + Menu theme.wav
    + Method to change music in backgroundMusic script
    + Debug Function (alphanumeric "1" key to change background music)
    * Modify BackgroundMusic gameobject to have less component

--- iso-space/Assets/scripts/BackgroundMusic.cs
+++ iso-space/Assets/scripts/BackgroundMusic.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class BackgroundMusic : MonoBehaviour {
 
-	public AudioSource normalMusic;
-	public AudioSource panicMusic;
+	public AudioClip music_Normal;
+	public AudioClip music_Panic;
+	AudioSource backgroundMusic;
+	// public AudioSource normalMusic;
+	// public AudioSource panicMusic;
 
 	bool isNormal = true;
 
 	// Use this for initialization
 	void Start () {
-		normalMusic.Play ();
+		backgroundMusic = GetComponent<AudioSource> ();
+		// backgroundMusic.clip = music_Normal;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		// Debugging function
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			isNormal = !isNormal;
+			ChangeMusic ();
+			backgroundMusic.Play ();
+		}
+	}
+
+	void ChangeMusic(){
+		if (isNormal == false)
+			backgroundMusic.clip = music_Panic;
+		else
+			backgroundMusic.clip = music_Normal;
 	}
 }
