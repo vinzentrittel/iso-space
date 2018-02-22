a803b017b0f12e0949337d59b5a8ab98aa538c71
Vinzent <vinzentrittel@uni-koblenz.de>
2018-02-22 01:38:51 +0100

    [+] Added dropping items from inventory

--- iso-space/Assets/scripts/InventoryController.cs
+++ iso-space/Assets/scripts/InventoryController.cs
@@ -6,49 +6,122 @@ using Item = ItemDatabase.Item;
 
 public class InventoryController : MonoBehaviour {
 
+	private const string PLAYER_NAME = "Player";
+
+	private Dictionary<int, TupleItem> dictItems;
+	private GameObject player;
+
+	[Tooltip("Number of instances allowed in the inventory")]
 	public int stackSize = 5;
 
-	private Dictionary<int, Item> items;
-	private Dictionary<int, int> numItems;
-
-	// Use this for initialization
-	void Start () {
-		items = new Dictionary<int, Item>();
-		numItems = new Dictionary<int, int>();
+	struct TupleItem {
+		public int	count;
+		public Item data;
+		public GameObject gObject;
 	}
 
 
-	public bool AddToInventory(Item item) {
+	void Start () {
+		dictItems = new Dictionary<int, TupleItem>();
+		player = GameObject.Find("Player");
+	}
+
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.Q))
+			DumpItem(1);
+
+		//Debug.Log(dictItems[1].count);
+	}
+	
+
+	public bool AddToInventory(Item dataItem, GameObject objItem) {		
 		// if an equal item is in this inventory
-		if(numItems.ContainsKey(item.ID)) {
-			// and their count exceeds the maximum size
-			if(numItems[item.ID] >= stackSize) {
-				// the item will not be stored
-				// TODO: print to UI
-				Debug.LogFormat("You cannot store more than {0} {1}s in this inventory", stackSize, item.Name);
+		if(dictItems.ContainsKey(dataItem.ID)) {
+			if(!AddToStack(dataItem.ID)) {
+				// TODO: show in UI
+				Debug.LogFormat("You cannot store more than {0} {1}s in this inventory", stackSize, dataItem.Name);
 				return false;
 			}
-		// if no other instancec of item is found
-		} else
-			// make a new slot for item
-			AddItem(item);
+			if(objItem != dictItems[dataItem.ID].gObject)
+				Destroy(objItem);
+			// if no other instancec of item is found
+		} else {
+			AddNewStack(dataItem, objItem);
+			Hide(objItem);
+		}
 
-		// another item for the stack
-		++numItems[item.ID];
 		return true;
 	}
 
-	public bool GetFromInventory(int id, out Item item) {
-		// find item
-		item = items.ContainsKey(id) ? items[id] : new Item();
-		// remove it
-		return items.Remove(id);
+
+	public void DumpItem(int id) {
+		GameObject objItem;
+		bool success;
+
+		objItem = RemoveFromInventory(id, out success);
+		if(!success) return;
+		
+		objItem.transform.position = player.transform.position;
+		Hide(objItem, true);
 	}
 
-	// Add the item data to the inventory
-	// The initial instance count in numItems is 0
-	void AddItem(Item item) {
-		items.Add(item.ID, item);
-		numItems.Add(item.ID, 0);
+
+	public GameObject RemoveFromInventory(int id, out bool success) {
+
+		GameObject item = null;
+		int count = 0;
+		success = false;
+
+		// find item
+		if(dictItems.ContainsKey(id)) {
+			// store item
+			item = dictItems[id].gObject;
+			// remove one item
+			RemoveFromStack(id);
+			if((count = dictItems[id].count) <= 0) {
+				// remove stored data, if no item remains
+				Debug.LogFormat("{0} remain.", dictItems[id].count);
+				dictItems.Remove(id);
+			}
+			// item successfully popped
+			success = true;
+		}
+		// return popped item, if any
+		return (count > 0) ? Instantiate(item) : item;
+	}
+
+
+	private void RemoveFromStack(int id) {
+		TupleItem temp = dictItems[id];
+		--temp.count;
+		dictItems[id] = temp;
+	}
+
+
+	private bool AddToStack(int id) {
+		TupleItem temp = dictItems[id];
+		if(temp.count >= stackSize)
+			return false;
+		
+		temp.count++;
+		dictItems[id] = temp; 
+		return true;
+	}
+
+
+	private void Hide(GameObject obj, bool unhide = false) {
+		obj.SetActive(unhide);
+		obj.hideFlags = unhide ? HideFlags.None : HideFlags.HideInInspector;
+	}
+
+
+	private void AddNewStack(Item dataItem, GameObject objItem) {
+		TupleItem newTuple = new TupleItem();
+		newTuple.count = 1;
+		newTuple.data = dataItem;
+		newTuple.gObject = objItem;
+
+		dictItems.Add(dataItem.ID, newTuple);
 	}
 }
--- iso-space/Assets/scripts/ItemController.cs
+++ iso-space/Assets/scripts/ItemController.cs
@@ -58,9 +58,8 @@ public class ItemController : MonoBehaviour {
 		if(other.gameObject.name == "Player")
 #endif
 		// try to store item in inventory
-		if(inventory.AddToInventory(item))
-			// if item is stored, destroy visual representation
-			Destroy(gameObject);
+		inventory.AddToInventory(item, gameObject);
+		
 	}
 
 
